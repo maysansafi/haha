a18b2c057322da8465d973ccb107e12be0912715
maysansafi <maysansafip@gmail.com>
2025-03-23 14:32:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var  change = document.querySelector(".change");
+
+console.log(change);
+function changeColorAndSize(){
+    var changge = document.querySelector(".ph");
+    console.log(changge);
+    change.textContent("يا مؤخرة");
+}
+change.onclick = changeColorAndSize;
